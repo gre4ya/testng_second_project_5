d6c883a1881618f6c077062d3685be9d3b0e6b0d
Andrii Grechyn <gre4ya@icloud.com>
2023-01-28 20:38:42 -0600

    created Test Case 2 and Test Case 3 in testng_second_project_5

--- src/test/java/pages/UnitedMainPage.java
+++ src/test/java/pages/UnitedMainPage.java
@@ -10,4 +10,17 @@ public class UnitedMainPage extends UnitedBasePage{
 
     @FindBy(css = "ul[class*='3RNBj'] li")
     public List<WebElement> bookTravelManuItems;
+    @FindBy(css = "div[class*='3WXQM'] input")
+    public List<WebElement> radioButtonsInput;
+    @FindBy(css = "label[class*='VrnUD']")
+    public List<WebElement> radioButtonsLabel;
+    @FindBy(id = "award")
+    public WebElement bookWithMilesCheckBoxInput;
+    @FindBy(css = "div[class*='2SGSV']>label")
+    public WebElement bookWithMilesCheckBoxLabel;
+    @FindBy(id = "flexibleDates")
+    public WebElement flexibleDatesCheckBoxInput;
+    @FindBy(css = "div[class*='2wMRQ']>label")
+    public WebElement flexibleDatesCheckBoxLabel;
+
 }
--- src/test/java/scripts/UnitedMainPageTest.java
+++ src/test/java/scripts/UnitedMainPageTest.java
@@ -21,13 +21,8 @@ public class UnitedMainPageTest extends UnitedBase{
      Given user is on "https://www.united.com/en/us"
      Then user should see "Book travel menu" navigation items
      | Book | Flight status | Check-in | My trips |
-
-     Test Case 3: Validate "Round-trip" and "One-way" radio buttons
-     Given user is on "https://www.united.com/en/us"
-     Then validate "Roundtrip" radio button is displayed, is enabled and is selected
-     And validate "One-way" radio button is displayed, is enabled but is not selected
      */
-    @Test(priority = 1, description = "Validate Book travel menu navigation items")
+    @Test(priority = 1, description = "Validation of Book travel menu navigation items")
     public void validateBookTravelManuNavigationItems(){
         String[] bookTravelManuItemsExpected = {
                 "Book",
@@ -39,5 +34,58 @@ public class UnitedMainPageTest extends UnitedBase{
             Assert.assertEquals(unitedMainPage.bookTravelManuItems.get(i).getText(),
                     bookTravelManuItemsExpected[i]);
         });
+    }
+    /**
+     Test Case 3: Validate "Round-trip" and "One-way" radio buttons
+     Given user is on "https://www.united.com/en/us"
+     Then validate "Roundtrip" radio button is displayed, is enabled and is selected
+     And validate "One-way" radio button is displayed, is enabled but is not selected
+     When user clicks on "One-way" radio button
+     Then validate "One-way" radio button is selected while "Roundtrip" radio button is
+     deselected
+     */
+    @Test(priority = 2, description = "Validation of 'Round-trip' and 'One-way' radio buttons")
+    public void validateRadioButtons(){
+        IntStream.range(0, unitedMainPage.radioButtonsInput.size()).forEach(i -> {
+            Assert.assertTrue(unitedMainPage.radioButtonsLabel.get(i).isDisplayed());
+            Assert.assertTrue(unitedMainPage.radioButtonsInput.get(i).isEnabled());
+        });
+        Assert.assertTrue(unitedMainPage.radioButtonsInput.get(0).isSelected());
+        Assert.assertFalse(unitedMainPage.radioButtonsInput.get(1).isSelected());
+        unitedMainPage.radioButtonsInput.get(1).click();
+        Assert.assertTrue(unitedMainPage.radioButtonsInput.get(1).isSelected());
+        Assert.assertFalse(unitedMainPage.radioButtonsInput.get(0).isSelected());
+    }
+    /**
+     Test Case 4: Validate "Book with miles" and "Flexible dates" checkboxes
+     Given user is on "https://www.united.com/en/us"
+     Then validate "Book with miles" checkbox is displayed, is enabled but is not selected
+     And validate "Flexible dates" checkbox is displayed, is enabled but is not selected
+     When user clicks both checkboxes
+     Then validate both checkboxes are selected
+     When user clicks on both selected checkboxes again
+     Then validate both checkboxes are deselected
+     */
+    @Test(priority = 3, description = "Validation of 'Book with miles' and 'Flexible dates' checkboxes")
+    public void validateCheckBoxes(){
+        Assert.assertTrue(unitedMainPage.bookWithMilesCheckBoxLabel.isDisplayed());
+        Assert.assertTrue(unitedMainPage.bookWithMilesCheckBoxInput.isEnabled());
+        Assert.assertFalse(unitedMainPage.bookWithMilesCheckBoxInput.isSelected());
+
+        Assert.assertTrue(unitedMainPage.flexibleDatesCheckBoxLabel.isDisplayed());
+        Assert.assertTrue(unitedMainPage.flexibleDatesCheckBoxInput.isEnabled());
+        Assert.assertFalse(unitedMainPage.flexibleDatesCheckBoxInput.isSelected());
+
+        unitedMainPage.flexibleDatesCheckBoxLabel.click();
+        unitedMainPage.bookWithMilesCheckBoxLabel.click();
+
+        Assert.assertTrue(unitedMainPage.bookWithMilesCheckBoxInput.isSelected());
+        Assert.assertTrue(unitedMainPage.flexibleDatesCheckBoxInput.isSelected());
+
+        unitedMainPage.flexibleDatesCheckBoxLabel.click();
+        unitedMainPage.bookWithMilesCheckBoxLabel.click();
+
+        Assert.assertFalse(unitedMainPage.bookWithMilesCheckBoxInput.isSelected());
+        Assert.assertFalse(unitedMainPage.flexibleDatesCheckBoxInput.isSelected());
     }
 }
